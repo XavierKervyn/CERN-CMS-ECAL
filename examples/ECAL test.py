a176d0bab4e588a6674909834cce33da8b9c9ded
ecalgit <ecalgit@cern.ch>
2022-06-29 13:27:58 +0200

    change test

--- examples/ECAL test.py
+++ examples/ECAL test.py
@@ -3,7 +3,7 @@ from ecalautoanalysis import *
 import numpy as np
 
 # ---- CHANGE HERE ----
-included_runs = [15680, 15681, 15682]
+included_runs = [15680]
 #included_runs = list(np.arange(15674, 15723+1))
 """
 included_runs.remove(15676)
@@ -28,13 +28,13 @@ all_channels_ = False
 variation_ = 'spill'
 spill_i_ = 3
 # ---- END OF CHANGES ----
-"""
-t.spill_variation(ref_channel_, all_channels_)
 
+t.spill_variation(ref_channel_, all_channels_)
+"""
 t.hist(ref_channel_, all_channels_, fit_option='synchronise')
-"""
+
 t.hist(ref_channel_, all_channels_, fit_option=None)
-"""
+
 #t.hist(ref_channel_, all_channels_, fit_option='gaussians', nb_fits=11)
 t.hist(ref_channel=ref_channel_, all_channels=all_channels_, variation=variation_, spill_i=spill_i_) #! variation='spill'
 
